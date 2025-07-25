259054e99c57d8b13324e64bc424e7f13fd4c8c2
WangQing <2917021186@qq.com>
2025-07-25 07:23:44 +0000

    [Ascend] impl cosh acos acosh

--- impl/ascend/functions/cos.cpp
+++ impl/ascend/functions/cos.cpp
@@ -19,5 +19,35 @@ diopiError_t diopiCos(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiCo
     return diopiSuccess;
 }
 
+diopiError_t diopiAcos(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
+    DIOPI_ASCEND_CALL_ACLNN(aclnnAcos, ctx, input, out);
+    return diopiSuccess;
+}
+
+diopiError_t diopiAcosInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
+    DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceAcos, ctx, input);
+    return diopiSuccess;
+}
+
+diopiError_t diopiCosh(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
+    DIOPI_ASCEND_CALL_ACLNN(aclnnCosh, ctx, input, out);
+    return diopiSuccess;
+}
+
+diopiError_t diopiCoshInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
+    DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceCosh, ctx, input);
+    return diopiSuccess;
+}
+
+diopiError_t diopiAcosh(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
+    DIOPI_ASCEND_CALL_ACLNN(aclnnAcosh, ctx, input, out);
+    return diopiSuccess;
+}
+
+diopiError_t diopiAcoshInp(diopiContextHandle_t ctx, diopiTensorHandle_t input){
+    DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceAcosh, ctx, input);
+    return diopiSuccess;
+}
+
 }  // namespace ascend
 }  // namespace impl
