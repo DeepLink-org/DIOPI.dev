40c2f36bf8b803f8040ef09b673b5144ce0e301f
WangQing <2917021186@qq.com>
2025-07-25 07:20:15 +0000

    [Ascend] impl sinh asin asinh

--- impl/ascend/functions/sin.cpp
+++ impl/ascend/functions/sin.cpp
@@ -18,5 +18,35 @@ diopiError_t diopiSinInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
     return diopiSuccess;
 }
 
+diopiError_t diopiAsin(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
+    DIOPI_ASCEND_CALL_ACLNN(aclnnAsin, ctx, input, out);
+    return diopiSuccess;
+}
+
+diopiError_t diopiAsinInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
+    DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceAsin, ctx, input);
+    return diopiSuccess;
+}
+
+diopiError_t diopiSinh(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
+    DIOPI_ASCEND_CALL_ACLNN(aclnnSinh, ctx, input, out);
+    return diopiSuccess;
+}
+
+diopiError_t diopiSinhInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
+    DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceSinh, ctx, input);
+    return diopiSuccess;
+}
+
+diopiError_t diopiAsinh(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
+    DIOPI_ASCEND_CALL_ACLNN(aclnnAsinh, ctx, input, out);
+    return diopiSuccess;
+}
+
+diopiError_t diopiAsinhInp(diopiContextHandle_t ctx, diopiTensorHandle_t input){
+    DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceAsinh, ctx, input);
+    return diopiSuccess;
+}
+
 }  // namespace ascend
 }  // namespace impl
